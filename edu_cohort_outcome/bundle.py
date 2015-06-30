6d42160298c122528cd4657d97cbbb5c25b5f094
roman <roman.suprotkin@developex.org>
2015-06-30 20:05:46 +0300

    fixed edu_cohort_outcome filetype

--- edu_cohort_outcome/bundle.py
+++ edu_cohort_outcome/bundle.py
@@ -5,8 +5,9 @@ from ambry.bundle.rowgen import DelimitedRowGenerator
 
 
 class Bundle(TsvBuildBundle):
-    def row_gen_for_source(self, source_name, use_row_spec = True):
-
-        fn = self.filesystem.download(source_name)
-
-        return DelimitedRowGenerator(fn, delimiter=self.delimiter)
+    # def row_gen_for_source(self, source_name, use_row_spec = True):
+    #
+    #     fn = self.filesystem.download(source_name)
+    #
+    #     return DelimitedRowGenerator(fn, delimiter=self.delimiter)
+    pass
