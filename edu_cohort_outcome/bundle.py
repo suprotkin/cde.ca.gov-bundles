3bc9fba9e8eb0b162dcb215b678b761c00b4b96b
roman <roman.suprotkin@developex.org>
2015-06-30 20:06:49 +0300

    cleaned bundle file

--- edu_cohort_outcome/bundle.py
+++ edu_cohort_outcome/bundle.py
@@ -1,13 +1,7 @@
 """"""
 
 from ambry.bundle.loader import TsvBuildBundle
-from ambry.bundle.rowgen import DelimitedRowGenerator
 
 
 class Bundle(TsvBuildBundle):
-    # def row_gen_for_source(self, source_name, use_row_spec = True):
-    #
-    #     fn = self.filesystem.download(source_name)
-    #
-    #     return DelimitedRowGenerator(fn, delimiter=self.delimiter)
     pass
