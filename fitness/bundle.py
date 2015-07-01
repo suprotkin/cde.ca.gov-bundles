5c64356881523019cc7d068428775f42558aa44b
roman <roman.suprotkin@developex.org>
2015-07-01 20:18:03 +0300

    Fitness bundle separated

--- fitness/bundle.py
+++ fitness/bundle.py
@@ -11,7 +11,7 @@ class Bundle(LoaderBundle):
             return None
         else:
             return v
-    
+
     def row_mangler(self, source, row_gen, row):
         """
         Override this function to alter each row of a source file, just after it is passed out of a Delimited file
@@ -19,14 +19,11 @@ class Bundle(LoaderBundle):
         :param row:
         :return:
         """
-        # print self.schema
-        # print source, row_gen, row
-        # raise
         if row_gen._header and len(row_gen._header) < len(row):
-            
-            if '\t' in row:
-                raise Exception() # tabs should be handled by filetype in the sources. 
 
-            i =  len(row_gen._header) -1
+            if '\t' in row:
+                raise Exception()  # tabs should be handled by filetype in the sources.
+
+            i = len(row_gen._header) - 1
             row = row[:i] + [','.join(row[i:])]
         return row
